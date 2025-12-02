273c7a60225a25aec3d72e14629bb708abc30afe
giorgiospagnolo21-wq <giorgiospagnolo21@gmail.com>
2025-12-02 12:02:41 +0100

    Import App.css in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import './App.css';
 function App() {
   const [token, setToken] = useState('');
   const [username, setUsername] = useState('');
