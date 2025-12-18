da0f41eb0fffcb812e63b95e8e2028c7fc638ed9
giorgiospagnolo21-wq <giorgiospagnolo21@gmail.com>
2025-12-18 14:23:12 +0100

    Replace BrowserRouter with HashRouter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import VotePage from './pages/VotePage';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/vote" element={<VotePage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
